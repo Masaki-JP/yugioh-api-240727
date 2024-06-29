8070189f2eff3b31373abf6682c2f45bd214d0cd
Masaki-JP <masakidayo0508@gmail.com>
2024-06-29 22:32:33 +0900

    add: SettingViewを静的実装

---
+++ Yu-Gi-Oh-API/SettingView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Coming soon...")
+                .navigationTitle("Settings")
+        }
+    }
+}
+
+#Preview {
+    SettingView()
+}
