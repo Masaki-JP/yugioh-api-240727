1561618cf53242df986d11e0be2f9fdd80a589a9
Masaki-JP <masakidayo0508@gmail.com>
2024-07-02 17:44:55 +0900

    feat: スクロールに対してレスポンシブなボタンを実装

--- Yu-Gi-Oh-API/HomeView.swift
+++ Yu-Gi-Oh-API/HomeView.swift
@@ -1,32 +1,51 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var isScrolling = false
+    @Namespace private var namespace
+
     var body: some View {
         HomeViewContent {
-            ScrollView {
-                LazyVStack {
-                    ScrollableCardRow("New Cards", lines: 2)
-                    ScrollableCardRow("Favorite Cards", lines: 2)
-                    ScrollableCardRow("All Cards", lines: 2)
+            HomeViewContentBody()
+                .safeAreaInset(edge: .bottom) {
+                    HStack(spacing: 0) {
+                        if isScrolling { Spacer() }
+                        getCardsButton.padding(.trailing, isScrolling ? 15 : 0)
+                    }
+                    .padding(.bottom, 10)
                 }
-            }
-            .safeAreaInset(edge: .bottom) {
-                getCardsButton
-                    .padding(.bottom)
-            }
+                .onPreferenceChange(IsScrollingPreferenceKey.self) { value in
+                    isScrolling = value
+                }
+                .animation(.easeInOut(duration: 0.1), value: isScrolling)
         }
     }
 
     var getCardsButton: some View {
-        Button(action: {}, label: {
-            Text("Let's get new card!!")
-                .fontWeight(.semibold)
-                .frame(height: 30)
-                .padding(.horizontal, 50)
-                .foregroundStyle(.white)
-                .background(Color.orange.gradient)
-                .clipShape(RoundedRectangle(cornerRadius: 5))
-        })
+        Button {} label: {
+            if isScrolling == false {
+                Text("Let's get new card!!")
+                    .font(.title2)
+                    .lineLimit(1)
+                    .fontWeight(.semibold)
+                    .frame(height: 40) // TotalHeight: 40
+                    .padding(.horizontal, 50)
+                    .foregroundStyle(.white)
+                    .background(.orange.gradient)
+                    .clipShape(.capsule)
+                    .matchedGeometryEffect(id: "getCardsButton", in: namespace)
+            } else {
+                Image(systemName: "plus")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.white)
+                    .frame(width: 20, height: 20)
+                    .padding(10) // TotalHeight: 50
+                    .background(.orange.gradient)
+                    .clipShape(.circle)
+                    .matchedGeometryEffect(id: "getCardsButton", in: namespace)
+            }
+        }
     }
 }
 
