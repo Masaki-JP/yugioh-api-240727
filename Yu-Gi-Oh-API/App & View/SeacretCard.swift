26ae9ef09817e287398f26d45c746306c5dfe5b4
Masaki-JP <masakidayo0508@gmail.com>
2024-07-03 15:39:09 +0900

    refactor: GetCardViewからSeacretCardを切り出し

---
+++ Yu-Gi-Oh-API/App & View/SeacretCard.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct SeacretCard: View {
+    @State private var isCovering = true
+
+    var body: some View {
+        Image(.darkMagician)
+            .resizable()
+            .scaledToFit()
+            .overlay {
+                if isCovering == true {
+                    cardCover
+                        .transition(.movingParts.glare)
+                        .onTapGesture {
+                            withAnimation {
+                                isCovering = false
+                            }
+                        }
+                }
+            }
+    }
+
+    var cardCover: some View {
+        GeometryReader {
+            Image(systemName: "questionmark")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.white)
+                .frame(width: $0.size.width*0.3)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(red: 0.3, green: 0.3, blue: 0.3))
+        }
+    }
+}
+
+#Preview {
+    SeacretCard()
+        .preferredColorScheme(.dark)
+}
