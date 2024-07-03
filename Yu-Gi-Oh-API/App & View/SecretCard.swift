92ff1dd602e6d65126e63a941de7d5abbb8f4c3d
Masaki-JP <masakidayo0508@gmail.com>
2024-07-03 23:06:24 +0900

    feat: SecretCardにてカード表示時の効果音を追加

--- Yu-Gi-Oh-API/App & View/SecretCard.swift
+++ Yu-Gi-Oh-API/App & View/SecretCard.swift
@@ -1,7 +1,20 @@
 import SwiftUI
+import AVFoundation
 
 struct SecretCard: View {
     @State private var isCovering = true
+    let invalidateSoundEffect: Bool
+    let audioPlayer: AVAudioPlayer?
+
+    init(invalidateSoundEffect: Bool = false) {
+        self.invalidateSoundEffect = invalidateSoundEffect
+        if let dataAsset = NSDataAsset(name: "CardFlip"),
+           let audioPlayer = try? AVAudioPlayer(data: dataAsset.data) {
+            self.audioPlayer = audioPlayer
+        } else {
+            self.audioPlayer = nil
+        }
+    }
 
     var body: some View {
         Image(.darkMagician)
@@ -11,11 +24,7 @@ struct SecretCard: View {
                 if isCovering == true {
                     cardCover
                         .transition(.movingParts.glare)
-                        .onTapGesture {
-                            withAnimation {
-                                isCovering = false
-                            }
-                        }
+                        .onTapGesture(perform: openCard)
                 }
             }
             .sensoryFeedback(.impact, trigger: isCovering)
@@ -30,6 +39,15 @@ struct SecretCard: View {
                 .frame(width: $0.size.width*0.3)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Color(red: 0.3, green: 0.3, blue: 0.3))
+        }
+    }
+
+    func openCard() {
+        withAnimation {
+            isCovering = false
+            if invalidateSoundEffect == false {
+                audioPlayer?.play()
+            }
         }
     }
 }
