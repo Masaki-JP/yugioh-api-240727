9b8db7064f94e23b4039ddd70a10c51698ae760f
Masaki-JP <masakidayo0508@gmail.com>
2024-07-05 03:09:48 +0900

    feat: CardListViewの動的実装

--- Yu-Gi-Oh-API/App & View/Core/CardListView.swift
+++ Yu-Gi-Oh-API/App & View/Core/CardListView.swift
@@ -1,18 +1,32 @@
 import SwiftUI
+import SwiftData
 
 struct CardListView: View {
+    @Query private var cards: [YDMCard]
+    private let columns = [GridItem(.adaptive(minimum: 70, maximum: 100))]
+
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: [.init(.adaptive(minimum: 70, maximum: 100))]) {
-                    ForEach(0..<30) { _ in
-                        Image(.darkMagician)
-                            .resizable().scaledToFit()
+                LazyVGrid(columns: columns) {
+                    ForEach(cards) { card in
+                        if let uiImage = UIImage(data: card.imageData) {
+                            Image(uiImage: uiImage)
+                                .resizable().scaledToFit()
+                        } else {
+                            Text("Error")
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        }
                     }
                 }
                 .padding(.horizontal)
             }
             .navigationTitle("All Cards")
+            .overlay {
+                if cards .isEmpty {
+                    ContentUnavailableView("No Cards", systemImage: "tray.fill", description: Text("You can get new cards from Home."))
+                }
+            }
         }
     }
 }
