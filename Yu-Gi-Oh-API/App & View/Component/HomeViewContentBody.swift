2ea93852fdb94e70bdfb1bf19c0c6f3278295153
Masaki-JP <masakidayo0508@gmail.com>
2024-07-05 21:40:08 +0900

    chore: HomeViewContentにランダムでカードを表示するScrollableCardRowを追加

--- Yu-Gi-Oh-API/App & View/Component/HomeViewContentBody.swift
+++ Yu-Gi-Oh-API/App & View/Component/HomeViewContentBody.swift
@@ -8,6 +8,7 @@ struct HomeViewContentBody: View {
             LazyVStack {
                 ScrollableCardRow("New Cards", lines: 2)
                 ScrollableCardRow("Favorite Cards", lines: 2)
+                ScrollableCardRow("Random Cards", lines: 2)
                 ScrollableCardRow("All Cards", lines: 2)
                 ScrollableCardRow("XXX Cards", lines: 3)
                 ScrollableCardRow("XXX Cards", lines: 3)
