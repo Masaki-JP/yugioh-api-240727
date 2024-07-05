4a7f917ea240aa73bccad780ae38b6a7a5377186
Masaki-JP <masakidayo0508@gmail.com>
2024-07-05 20:26:03 +0900

    feat: CardListViewにてカードの削除機能を実装

--- Yu-Gi-Oh-API/App & View/Core/CardListView.swift
+++ Yu-Gi-Oh-API/App & View/Core/CardListView.swift
@@ -3,6 +3,9 @@ import SwiftData
 
 struct CardListView: View {
     @Query private var cards: [YDMCard]
+    @Environment(\.modelContext) private var modelContext
+    @State private var isEditing = false
+    @State private var selectedCards: Set<YDMCard> = .init()
     private let columns = [GridItem(.adaptive(minimum: 70, maximum: 100))]
 
     var body: some View {
@@ -17,6 +20,20 @@ struct CardListView: View {
                                 Image(uiImage: uiImage)
                                     .resizable().scaledToFit()
                             }
+                            .disabled(isEditing == true)
+                            .overlay {
+                                if isEditing == true, selectedCards.contains(card) {
+                                    selectedItemCover
+                                }
+                            }
+                            .onTapGesture {
+                                guard isEditing == true else { return }
+                                if selectedCards.contains(card) == false {
+                                    selectedCards.insert(card)
+                                } else {
+                                    selectedCards.remove(card)
+                                }
+                            }
                         } else {
                             Text("Error")
                                 .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -25,12 +42,53 @@ struct CardListView: View {
                 }
                 .padding(.horizontal)
             }
-            .navigationTitle("All Cards")
-            .toolbarTitleDisplayMode(.inlineLarge)
+            .scrollDisabled(cards.isEmpty == true)
             .overlay {
                 if cards .isEmpty {
                     ContentUnavailableView("No Cards", systemImage: "tray.fill", description: Text("You can get new cards from Home."))
                 }
+            }
+            .navigationTitle("All Cards")
+            .toolbarTitleDisplayMode(.inlineLarge)
+            .toolbar(content: toolbarContent)
+            .sensoryFeedback(.impact, trigger: isEditing)
+            .sensoryFeedback(.decrease, trigger: selectedCards)
+        }
+    }
+
+    var selectedItemCover: some View {
+        GeometryReader {
+            Color.black.opacity(0.3)
+            Image(systemName: "checkmark.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: $0.size.width * 0.3)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .foregroundStyle(.green)
+        }
+    }
+
+    @ToolbarContentBuilder
+    func toolbarContent() -> some ToolbarContent {
+        ToolbarItemGroup(placement: .topBarTrailing) {
+            if isEditing {
+                Button("Delete", role: .destructive) {
+                    withAnimation {
+                        selectedCards.forEach {
+                            modelContext.delete($0)
+                        }
+                        selectedCards.removeAll()
+                        isEditing = false
+                    }
+                }
+                .tint(.red)
+                .disabled(selectedCards.isEmpty == true)
+            }
+            Button(isEditing == false ? "Edit" : "Done") {
+                if isEditing == true {
+                    selectedCards.removeAll()
+                }
+                isEditing.toggle()
             }
         }
     }
