155101af47e90c495d05159646d3280663168582
Masaki-JP <masakidayo0508@gmail.com>
2024-07-07 13:58:10 +0900

    fix: アプリが動作が重くなる問題を修正（以下詳細）

--- Yu-Gi-Oh-API/App & View/Component/HomeViewContentBody.swift
+++ Yu-Gi-Oh-API/App & View/Component/HomeViewContentBody.swift
@@ -2,17 +2,18 @@ import SwiftUI
 
 struct HomeViewContentBody: View {
     @StateObject private var scrollObserver = ScrollObserver()
+    @State private var sampleCards = getSampleCards(50)
 
     var body: some View {
         ScrollView {
             LazyVStack {
-                ScrollableCardRow("New Cards", cards: getSampleCards(10))
-                ScrollableCardRow("Favorite Cards", cards: getSampleCards(10))
-                ScrollableCardRow("Random Cards", cards: getSampleCards(20))
-                ScrollableCardRow("All Cards", cards: getSampleCards(20))
-                ScrollableCardRow("XXX Cards", cards: getSampleCards(30))
-                ScrollableCardRow("XXX Cards", cards: getSampleCards(40))
-                ScrollableCardRow("XXX Cards", cards: getSampleCards(50))
+                ScrollableCardRow("New Cards", cards: .init(sampleCards[0..<10]))
+                ScrollableCardRow("Favorite Cards", cards: .init(sampleCards[0..<10]))
+                ScrollableCardRow("Random Cards", cards: .init(sampleCards[0..<20]))
+                ScrollableCardRow("All Cards", cards: .init(sampleCards[0..<20]))
+                ScrollableCardRow("XXX Cards", cards: .init(sampleCards[0..<30]))
+                ScrollableCardRow("XXX Cards", cards: .init(sampleCards[0..<40]))
+                ScrollableCardRow("XXX Cards", cards: .init(sampleCards[0..<50]))
             }
             .background {
                 GeometryReader { geometry in
