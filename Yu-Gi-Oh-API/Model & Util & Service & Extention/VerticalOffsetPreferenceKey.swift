19c3ce901df59e589be1cd6885a4c27fcc321774
Masaki-JP <masakidayo0508@gmail.com>
2024-07-14 20:15:16 +0900

    remove: VerticalOffsetPreferenceKeyを削除

--- Yu-Gi-Oh-API/Model & Util & Service & Extention/VerticalOffsetPreferenceKey.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-struct VerticalOffsetPreferenceKey: PreferenceKey {
-    nonisolated(unsafe) static var defaultValue = CGFloat.zero
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {}
-}
