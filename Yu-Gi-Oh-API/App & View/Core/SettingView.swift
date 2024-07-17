f78609770f95a757a6808d19babb2cab86e87772
Masaki-JP <masakidayo0508@gmail.com>
2024-07-17 22:53:29 +0900

    feat: SettingViewにてデータの全削除を実装

--- Yu-Gi-Oh-API/App & View/Core/SettingView.swift
+++ Yu-Gi-Oh-API/App & View/Core/SettingView.swift
@@ -1,11 +1,26 @@
 import SwiftUI
+import SwiftData
 
 struct SettingView: View {
+    @Environment(\.modelContext) private var modelContext
+    @State private var isShowingAlert = false
+
     var body: some View {
         NavigationStack {
-            Text("Coming soon...")
-                .navigationTitle("Settings")
-                .toolbarTitleDisplayMode(.inlineLarge)
+            Form {
+                Button("データの全削除", role: .destructive) {
+                    isShowingAlert = true
+                }
+            }
+            .alert("確認", isPresented: $isShowingAlert) {
+                Button("削除する", role: .destructive) {
+                    try! modelContext.delete(model: YDMCard.self) // ⚠️
+                }
+            } message: {
+                Text("全てのデータを削除しますか？")
+            }
+            .navigationTitle("Settings")
+            .toolbarTitleDisplayMode(.inlineLarge)
         }
     }
 }
