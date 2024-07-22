bd4d01a447f98e45c9fdc8d30c2b72f301ecd029
Masaki-JP <masakidayo0508@gmail.com>
2024-07-23 00:05:57 +0900

    add: YuGiOhAPIClient.fetchSpecialCardsを定義

--- Yu-Gi-Oh-API/Model & Util & Service & Extention/YuGiOhAPIClient.swift
+++ Yu-Gi-Oh-API/Model & Util & Service & Extention/YuGiOhAPIClient.swift
@@ -47,3 +47,71 @@ struct YuGiOhAPIClient {
         }
     }
 }
+
+extension YuGiOhAPIClient {
+    func fetchSpecialCards() async -> [YDMCard.DTO]? {
+        let cardInfoList: [(name: String, cardImagesIndex: Int)] = [
+            ("Dark Magician", 2), ("Dark Magician Girl", 0), ("Blue-Eyes White Dragon", 0), ("Blue-Eyes Ultimate Dragon", 0), ("Red-Eyes Black Dragon", 1), ("Relinquished", 0), ("XYZ-Dragon Cannon", 1), ("Jinzo", 0), ("Summoned Skull", 0), ("Dark Necrofear", 0), ("Monster Reborn", 1), ("Axe of Despair", 0), ("Polymerization", 0), ("Harpie's Feather Duster", 0), ("Swords of Revealing Light", 0), ("Mirror Force", 0), ("Destiny Board", 0), ("Call of the Haunted", 0), ("Magic Cylinder", 0), ("Skull Dice", 0)
+        ]
+
+        return await withTaskGroup(of: YDMCard.DTO?.self) { group in
+            cardInfoList.forEach { cardInfo in
+                group.addTask {
+                    await fetchSpecialCard(name: cardInfo.name, cardImagesIndex: cardInfo.cardImagesIndex)
+                }
+            }
+
+            let cardDTOs: [YDMCard.DTO] = await group.compactMap { $0 }.reduce(into: [YDMCard.DTO]()) { $0.append($1) }
+            let sortedCardDTOs: [YDMCard.DTO] = cardInfoList.compactMap { cardInfo in
+                cardDTOs.filter { cardInfo.name == $0.name }.first
+            }
+
+            guard sortedCardDTOs.count == cardInfoList.count else { return nil }
+            return sortedCardDTOs
+        }
+
+        @Sendable func fetchSpecialCard(name: String, cardImagesIndex: Int) async -> YDMCard.DTO? {
+            guard
+                let url = URL(string: "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + name),
+                let (data, _) = try? await URLSession.shared.data(from: url),
+                let responseDTO2 = try? JSONDecoder().decode(ResponseDTO.self, from: data),
+                let responseDTO = responseDTO2.data.first,
+
+                responseDTO.card_images.count >= cardImagesIndex,
+                case let normalSizeImageURLString = responseDTO.card_images[cardImagesIndex].image_url,
+                let normalSizeImageURL = URL(string: normalSizeImageURLString),
+                let (normalSizeImageData, _) = try? await URLSession.shared.data(from: normalSizeImageURL),
+                let uiImage = UIImage(data: normalSizeImageData),
+                let normalSizeImageData = uiImage.jpegData(compressionQuality: 0.0),
+
+                case let smallSizeImageURLString = responseDTO.card_images[cardImagesIndex].image_url_small,
+                let smallSizeImageURL = URL(string: smallSizeImageURLString),
+                let (smallSizeImageData, _) = try? await URLSession.shared.data(from: smallSizeImageURL),
+                let uiImage = UIImage(data: smallSizeImageData),
+                let smallSizeImageData = uiImage.jpegData(compressionQuality: 0.0)
+
+            else { return nil }
+
+            return .init(
+                name: responseDTO.name,
+                normalSizeImageData: normalSizeImageData,
+                smallSizeImageData: smallSizeImageData,
+                isFavorite: true
+            )
+        }
+
+        struct ResponseDTO: Decodable {
+            let data: [Self.Data]
+
+            struct Data: Decodable {
+                let name: String
+                let card_images: [CardImage]
+
+                struct CardImage: Decodable {
+                    let image_url: String
+                    let image_url_small: String
+                }
+            }
+        }
+    }
+}
