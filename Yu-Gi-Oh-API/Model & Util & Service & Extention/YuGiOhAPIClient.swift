732d0c727332a8aeca79504da75000364b1943c6
Masaki-JP <masakidayo0508@gmail.com>
2024-07-23 00:02:52 +0900

    chore: YuGiOhAPIClient内の_fetch関数とResponseDTOの定義をfetchCards関数内に変更

--- Yu-Gi-Oh-API/Model & Util & Service & Extention/YuGiOhAPIClient.swift
+++ Yu-Gi-Oh-API/Model & Util & Service & Extention/YuGiOhAPIClient.swift
@@ -11,39 +11,39 @@ struct YuGiOhAPIClient {
             let cards = await group.compactMap { $0 }.reduce(into: [YDMCard.DTO]()) { $0.append($1) }
             return cards.count == numberOfCards ? cards : nil
         }
-    }
 
-    @Sendable private func _fetch() async -> YDMCard.DTO? {
-        guard
-            let url = URL(string: "https://db.ygoprodeck.com/api/v7/randomcard.php"),
-            let (data, _) = try? await URLSession.shared.data(from: url),
-            let responseDTO = try? JSONDecoder().decode(ResponseDTO.self, from: data),
-            let normalSizeImageURLString = responseDTO.card_images.first?.image_url,
-            let normalSizeImageURL = URL(string: normalSizeImageURLString),
-            let (normalSizeImageData, _) = try? await URLSession.shared.data(from: normalSizeImageURL),
-            let uiImage = UIImage(data: normalSizeImageData),
-            let normalSizeImageData = uiImage.jpegData(compressionQuality: 0.0),
-            let smallSizeImageURLString = responseDTO.card_images.first?.image_url_small,
-            let smallSizeImageURL = URL(string: smallSizeImageURLString),
-            let (smallSizeImageData, _) = try? await URLSession.shared.data(from: smallSizeImageURL),
-            let uiImage = UIImage(data: smallSizeImageData),
-            let smallSizeImageData = uiImage.jpegData(compressionQuality: 0.0)
-        else { return nil }
+        @Sendable func _fetch() async -> YDMCard.DTO? {
+            guard
+                let url = URL(string: "https://db.ygoprodeck.com/api/v7/randomcard.php"),
+                let (data, _) = try? await URLSession.shared.data(from: url),
+                let responseDTO = try? JSONDecoder().decode(ResponseDTO.self, from: data),
+                let normalSizeImageURLString = responseDTO.card_images.first?.image_url,
+                let normalSizeImageURL = URL(string: normalSizeImageURLString),
+                let (normalSizeImageData, _) = try? await URLSession.shared.data(from: normalSizeImageURL),
+                let uiImage = UIImage(data: normalSizeImageData),
+                let normalSizeImageData = uiImage.jpegData(compressionQuality: 0.0),
+                let smallSizeImageURLString = responseDTO.card_images.first?.image_url_small,
+                let smallSizeImageURL = URL(string: smallSizeImageURLString),
+                let (smallSizeImageData, _) = try? await URLSession.shared.data(from: smallSizeImageURL),
+                let uiImage = UIImage(data: smallSizeImageData),
+                let smallSizeImageData = uiImage.jpegData(compressionQuality: 0.0)
+            else { return nil }
 
-        return .init(
-            name: responseDTO.name,
-            normalSizeImageData: normalSizeImageData,
-            smallSizeImageData: smallSizeImageData
-        )
-    }
+            return .init(
+                name: responseDTO.name,
+                normalSizeImageData: normalSizeImageData,
+                smallSizeImageData: smallSizeImageData
+            )
+        }
 
-    private struct ResponseDTO: Decodable {
-        let name: String
-        let card_images: [CardImage]
+        struct ResponseDTO: Decodable {
+            let name: String
+            let card_images: [CardImage]
 
-        struct CardImage: Decodable {
-            let image_url: String
-            let image_url_small: String
+            struct CardImage: Decodable {
+                let image_url: String
+                let image_url_small: String
+            }
         }
     }
 }
